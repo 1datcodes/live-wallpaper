ac576fa95cde44e37161f222609832e93a893ca5
michi <tanakamichi7@gmail.com>
2024-10-25 19:07:05 -0700

    Finished font picking

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,12 +1,13 @@
 import { useState, useEffect } from 'react';
 import '../styles/Clock.css';
-import fontList from '../assets/google-fonts.json';
+// import fontList from '../assets/google-fonts.json';
 
 const key = import.meta.env.VITE_GOOGLE_FONT_API_KEY;
 
 function Clock() {
     const [date, setDate] = useState(new Date());
-    const [font, setFont] = useState('Roboto');
+    const [font, setFont] = useState(localStorage.getItem('currentFont') || 'Roboto');
+    const [fontList, setFontList] = useState({});
     
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -20,18 +21,37 @@ function Clock() {
     }, []);
 
     useEffect(() => {
-        const fontUrl = fontList[font]?.variants?.normal?.['400']?.url?.woff2;
-        if (fontUrl) {
-            const link = document.createElement('link');
-            link.href = fontUrl;
-            link.rel = 'stylesheet';
-            document.head.appendChild(link);
-
-            return () => {
-                document.head.removeChild(link);
-            }
+        const fetchFonts = async () => {
+            try {
+                const response = await fetch(`https://www.googleapis.com/webfonts/v1/webfonts?key=${key}`);
+                const data = await response.json();
+                const fonts = data.items.reduce((acc, font) => {
+                    acc[font.family] = font;
+                    return acc;
+                }, {});
+                setFontList(fonts);
+            } catch (error) {
+                console.error(error);
+            };
         }
+
+        fetchFonts();
+    }, []);
+
+    useEffect(() => {
+        const fontURL = `https://fonts.googleapis.com/css2?family=${font.replaceAll(/ /g, '+')}&display=swap`;
+        const link = document.createElement('link');
+        link.href = fontURL;
+        link.rel = 'stylesheet';
+        document.head.appendChild(link);
+
+        return () => document.head.removeChild(link);
     }, [font]);
+
+    const saveCurrentFont = (fontName) => {
+        localStorage.setItem('currentFont', fontName);
+        setFont(fontName);
+    }
 
     const fontOptions = Object.keys(fontList).map(fontName => (
         <option key={fontName} value={fontName} style={{fontFamily: fontName}}>{fontName}</option>
@@ -47,7 +67,7 @@ function Clock() {
             </div>
 
             <div className="Font-picker">
-                <select value={font} onChange={e => setFont(e.target.value)}>
+                <select value={font} onChange={e => saveCurrentFont(e.target.value)}>
                     {fontOptions}
                 </select>
             </div>
